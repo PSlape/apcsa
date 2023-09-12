c1bfd63be47270f71bce5297665b3b41ca4383ed
Peyton Slape <pey.slape@gmail.com>
2023-09-11 23:12:41 -0700

    7.3

--- DrawingTest.java
+++ DrawingTest.java
@@ -1,7 +1,8 @@
-// L.A.6.2
+// L.A.6.2 and L.A.7.3
+import java.lang.Double;
 
 public class DrawingTest {
-    public static void main(String[] args) {
+    public static void main_lab6() {
         Rectangle r1 = new Rectangle(-60,60,60,60);
         Rectangle r2 = new Rectangle(0,60,60,60);
         Rectangle r3 = new Rectangle(60,60,60,60);
@@ -21,5 +22,24 @@ public class DrawingTest {
         r7.draw();
         r8.draw();
         r9.draw();
+    }
+    public static void main_lab7() {
+        Rectangle rect = new Rectangle(0, 0, 10, 20);
+        for(int i = 0; i < 5; i++) {
+            rect.setDirection(rect.getDirection() + 36);
+            rect.setHeight(rect.getHeight() + 5);
+            rect.draw();
+        }
+        
+        Rectangle r1 = new Rectangle(rect);
+        
+        r1.setX(-200);
+        r1.setY(200);
+        
+        r1.draw();
+        r1.drawString("Width = " + r1.getWidth(), -200, 150);
+        r1.drawString("Height = " + r1.getHeight(), -200, 130);
+        r1.drawString("Area = " + r1.getArea(), -200, 110);
+        r1.drawString("Perimeter = " + r1.getPerimeter(), -200, 90);
     }
 }
--- Rectangle.java
+++ Rectangle.java
@@ -10,10 +10,9 @@ import apcslib.*;
 public class Rectangle {
     private static DrawingTool dw = new DrawingTool(new SketchPad(500,500));
     
-    private double perim, area, x, y, width, height;
+    private double x, y, width, height, direction;
     
-    public Rectangle()
-    {
+    public Rectangle() {
      this(0,0,0,0);   
     }
     public Rectangle(double x, double y, double width, double height) {
@@ -22,16 +21,30 @@ public class Rectangle {
         this.width = width;
         this.height = height;
     }
+    public Rectangle(double x, double y, double width, double height, double offset) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.direction = offset;
+    }
+    public Rectangle(Rectangle r) {
+        this.x = r.x;
+        this.y = r.y;
+        this.width = r.width;
+        this.height = r.height;
+        this.direction = r.direction;
+    }
     public double getPerimeter() {
-        return perim;
+        return (2 * width) + (2 * height);
     }
     public double getArea() {
-        return area;
+        return width * height;
     }
     public void draw() {
         dw.up(); // disables drawing mode
         dw.move(x-width, y-height); // moves to the bottom left corner of the shape // gets the direction the drawing tool is facing
-        dw.setDirection(90); // sets the direction to 90
+        dw.setDirection(direction); // sets the direction to 90
         dw.down();
         
         int sideCounter = 0;
@@ -44,5 +57,42 @@ public class Rectangle {
             dw.turnRight(90);
             sideCounter++;
         }
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setHeight(double h) {
+        height = h;
+    }
+    public void setWidth(double w) {
+        width = w;
+    }
+    public void setDirection(double dir) {
+        direction = dir;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public double getDirection() {
+        return direction;
+    }
+    public void drawString(String str, double x, double y) {
+        dw.up();
+        dw.move(x, y);
+        dw.down();
+        dw.setDirection(0);
+        dw.drawString(str);
     }
 }
