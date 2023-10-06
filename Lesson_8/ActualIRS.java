845e465ac3dc210cfc30cd6f3c893e00dcbc4732
Peyton S <pey.slape@gmail.com>
2023-10-06 10:01:36 -0700

    ah

--- Lesson_8/ActualIRS.java
+++ Lesson_8/ActualIRS.java
@@ -3,8 +3,12 @@ package Lesson_8;
 
 import chn.util.ConsoleIO;
 import apcslib.Format;
-import java.lang.Exception;
-
+/*
+ * Name: Peyton Slape
+ * Description: Gets your taxes
+ * Lab: 8.1
+ * Purpose: 
+ */
 public class ActualIRS {
     public static void main(String[] args) {
         ConsoleIO cons = new ConsoleIO();
@@ -26,14 +30,21 @@ class Taxpayer {
     private double income;
     private String status;
     
+        // Default constructor sets status to single and income to 0
     public Taxpayer() {
         income = 0;
         status = "single";
     }
+    
+            // Constructor
     public Taxpayer(String status, double income) {
         this.status = status;
         this.income = income;
     }
+    /*
+    * If married, uses the marriedTax private method, if single, then it uses the singleTax private method
+    * Returns the value from either of the tax methods, otherwise returns 0
+    */ 
     double calculateTax() {
         if(status.equalsIgnoreCase("married")) {
             return marriedTax(income);
@@ -42,33 +53,41 @@ class Taxpayer {
         }
         return 0;
     }
+    
+    /*
+     * Calculates tax for a couple based on the constants from the Constants class
+     */
     private static double marriedTax(double income){
         if(income >= Constants.TAX_E_MIN) {
-            return Constants.TAX_E_FLAT_M + (Constants.TAX_E_PER * income);
+            return Constants.TAX_E_FLAT_M + (Constants.TAX_E_PER * income); // Bracket 5
         } else if(income >= Constants.TAX_D_MIN_M) {
-            return Constants.TAX_D_FLAT_M + (Constants.TAX_D_PER * income);
+            return Constants.TAX_D_FLAT_M + (Constants.TAX_D_PER * income); // Bracket 4
         } else if(income >= Constants.TAX_C_MIN_M) {
-            return Constants.TAX_C_FLAT_M + (Constants.TAX_C_PER * income);
+            return Constants.TAX_C_FLAT_M + (Constants.TAX_C_PER * income); // Bracket 3
         } else if(income >= Constants.TAX_B_MIN_M) {
-            return Constants.TAX_B_FLAT_M + (Constants.TAX_B_PER * income);
+            return Constants.TAX_B_FLAT_M + (Constants.TAX_B_PER * income); // Bracket 2
         } else if(income >= 0) {
-            return Constants.TAX_A * income;
+            return Constants.TAX_A * income; // Tax Bracket 1
         }
         return 0;
     }
+    
+    /*
+     * Calculates tax for a single person based on the constants from the Constants class
+     */
     private static double singleTax(double income) {
         if(income >= Constants.TAX_E_MIN) {
-            return Constants.TAX_E_FLAT_S + (Constants.TAX_E_PER * income);
+            return Constants.TAX_E_FLAT_S + (Constants.TAX_E_PER * income); // Bracket 5
         } else if(income >= Constants.TAX_D_MIN_S) {
-            return Constants.TAX_D_FLAT_S + (Constants.TAX_D_PER * income);
+            return Constants.TAX_D_FLAT_S + (Constants.TAX_D_PER * income); // Bracket 4
         } else if(income >= Constants.TAX_C_MIN_S) {
-            return Constants.TAX_C_FLAT_S + (Constants.TAX_C_PER * income);
+            return Constants.TAX_C_FLAT_S + (Constants.TAX_C_PER * income); // Bracket 3
         } else if(income >= Constants.TAX_B_MIN_S) {
-            return Constants.TAX_B_FLAT_S + (Constants.TAX_B_PER * income);
+            return Constants.TAX_B_FLAT_S + (Constants.TAX_B_PER * income); // Bracket 2
         } else if(income >= 0) {
-            return Constants.TAX_A * income;
+            return Constants.TAX_A * income; // Tax Bracket 1
         }
-        return 0;
+        return 0; // returns zero if there is an error
     }
     
 }
--- Lesson_8/Constants.java
+++ Lesson_8/Constants.java
@@ -1,26 +1,40 @@
 package Lesson_8;
-public class Constants
-{
+public class Constants {
+    /*
+     * S is for single
+     * M is for married
+     * PER is the percent of tax to add
+     * FLAT is the flat amount of tax to add
+     * MIN is for minimum
+     * MAX is for maximum
+     */
+    
+            // Constant for the first tax bracket
     public static final double TAX_A = 0.15;
     
+    
+            // Constants for the second tax bracket
     public static final double TAX_B_MIN_S = 27050;
     public static final double TAX_B_MIN_M = 45200;
     public static final double TAX_B_PER = 0.275;
     public static final double TAX_B_FLAT_S = 4057.50;
     public static final double TAX_B_FLAT_M = 6780;
     
+            // Constants for the third tax bracket
     public static final double TAX_C_MIN_S = 65550;
     public static final double TAX_C_MIN_M = 109250;
     public static final double TAX_C_PER = 0.305;
     public static final double TAX_C_FLAT_S = 14645;
     public static final double TAX_C_FLAT_M = 24393.75;
     
+            // Constants for the fourth tax bracket
     public static final double TAX_D_MIN_S = 136750;
     public static final double TAX_D_MIN_M = 166500;
     public static final double TAX_D_PER = 0.355;
     public static final double TAX_D_FLAT_S = 36361;
     public static final double TAX_D_FLAT_M = 41855;
-    
+
+            // Constants for the fifth tax bracket
     public static final double TAX_E_MIN = 297350;
     public static final double TAX_E_PER = 0.391;
     public static final double TAX_E_FLAT_S = 93374;
