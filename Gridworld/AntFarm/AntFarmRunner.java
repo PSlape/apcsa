394452d9003b8584cadc40ca801b3d88bd2c8e48
Peyton S <mudzgamer5557@outlook.com>
2024-02-14 10:48:45 -0800

    more food

--- Gridworld/AntFarm/AntFarmRunner.java
+++ Gridworld/AntFarm/AntFarmRunner.java
@@ -34,18 +34,20 @@ import info.gridworld.grid.Location;
  */
 public class AntFarmRunner
 {
-	public static void main(String[] args)
-	{
-		Grid<Actor> grid = new BoundedGrid<Actor>(19, 19);
+    public static void main(String[] args)
+    {
+        Grid<Actor> grid = new BoundedGrid<Actor>(19, 19);
         ActorWorld world = new ActorWorld(grid);
 
         world.add(new Location(2, 2), new Cookie());
+        world.add(new Location(2, 18), new Cookie());
         world.add(new Location(17, 2), new Cake());
+
         world.add(new Location(9, 15), new QueenAnt());
 
         for (int i = 0; i < 25; i++)
-        	world.add(new WorkerAnt());
+            world.add(new WorkerAnt());
 
         world.show();
-	}
+    }
 }
